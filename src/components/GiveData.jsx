2edef6132c0c727bf5b69ad6a2fce1cb30a93a73
sarina <sarinaem75@gmail.com>
2024-12-11 19:08:02 +0330

    put api in file by .env

--- src/components/GiveData.jsx
+++ src/components/GiveData.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyBu_BolUMVaxNo7eQh4UXolWfOy4mH3CYA";
+const API_KEY = process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY;
+
 const API_URL = "https://translation.googleapis.com/language/translate/v2"
 const GiveData = async (text, targetLanguage) => {
   const response = await axios.post(
