5b777167c0cd4218ef4f8d6cf5c156c107d0ca3f
sarina <sarinaem75@gmail.com>
2024-12-18 16:22:05 +0330

    [seprate] userChat from NewTopic
    [add] edit in userChat

--- src/components/NewTopic.jsx
+++ src/components/NewTopic.jsx
@@ -1,18 +1,18 @@
 
-import sendIcon from "../assets/Send.svg";
 import HeaderChatbot from "./HeaderChatbot";
 import { useState } from "react";
-import Profile from "../assets/Profile.svg";
 import copy from "../assets/copy.svg";
-import edit from "../assets/edit.svg"
 import ReactImg from "../assets/ReactImg.svg";
 import shareContent from "../assets/shareContent.svg"
 import ItemTopic from "./ItemTopic";
+import InputField from "./InputField";
+import UserChat from "./UserChat";
 
 
  export default function NewTopic() {
   const [inputValue, setInputValue] = useState("");
   const [transtMsg, setTransMsg] = useState("");
+  // const [loading, setLoading] = useState(false); add the last time
 
   const [msg, setMsg] = useState("");
   const handleMsg = async (inputValue) => {
@@ -32,10 +32,6 @@ import ItemTopic from "./ItemTopic";
         } else {
           setTransMsg("definition not found.")          
         }
-           
-        // definitions.forEach(definition => {
-        //   console.log(definition.definition);  
-        // });
       })
 
     }catch(error){
@@ -58,53 +54,24 @@ import ItemTopic from "./ItemTopic";
               <ItemTopic title="Explain why popcorn pops" description="to a kid who loves watching it in the microwave"/>
             </div>
         </div>
-        <div className="flex flex-row gap-3 items-center justify-center">
-          <input 
-          type="text"
-          value={inputValue}
-          name="input"
-          onChange={(event) =>setInputValue(event.target.value)}
-          onKeyDown={(e)=>{
-            if(e.key === "Enter") {
-              handleMsg(inputValue);
-            }
-          }}
-           placeholder="Ask me anything..." className=" w-[70%] rounded-[30px] border border-1 py-4 px-6 border-[#EBEDEC] ml-6 mb-6"/>
-             <button onClick={() => handleMsg(inputValue)}><img src={sendIcon} alt="send" className=" bg-[#01CD98] p-4 rounded-full flex gap-2 mr-6 mb-6" />
-            </button>
-           
-        </div>
+        
+        <InputField inputValue={inputValue} setInputValue={setInputValue} handleMsg={handleMsg}/>
         <div className="border border-[#EBEDEC] mt-10"></div>
 
         {msg && (
 
         <div>
-           <div className="flex gap-3 mt-6 mb-6">
-            <img src={Profile} alt="profile" className="w-6 h-6 ml-6 " />
-            <p className="">you</p>
-           </div>
-           <p className="mr-6 ml-6 mt-4 text-[#01CD98] font-sans font-normal text-base leading-6">{msg}</p>
-          <div className="flex gap-6 flex-row ml-6 mt-4">
-            <div className="flex gap-2">
-            <img className="w-4 h-4" src={edit} alt="edit" />
-            <p className="text-[#616161] font-medium text-sm leading-[18px] font-sans">Edit</p>
-          </div>
-          <div className="flex gap-2">
-            <img className="w-4 h-4" src={copy} alt="copy" />
-            <p className="text-[#616161] font-medium text-sm leading-[18px] font-sans">Copy</p>
-          </div>
-        </div>
+          <UserChat msg={msg}/>
          <div className="border border-[#EBEDEC] mt-10"></div>
-{transtMsg && (
-  <div>
+  {transtMsg && (
+  <div className="mb-12">
     <div className="flex gap-3 ml-6 mt-6 items-center">   
      <img src={ReactImg} alt="React Logo" className="w-6 h-6" />
-     <h1 className=" font-semibold text-base leading-5 font-sans text-center whitespace-nowrap">Chat Bot:</h1>
+     <h1 className="font-semibold text-base leading-5 font-sans text-center whitespace-nowrap">Chat Bot:</h1>
    </div>
    <p className="ml-7 mt-4 mr-6 text-[#051320] font-normal text-base leading-6">{transtMsg}</p>
    <div className="flex gap-6 flex-row ml-6 mt-4">
      <div className="flex gap-2">
-     <img className="w-4 h-4" src={copy} alt="" />
 
        <img className="w-4 h-4" src={shareContent} alt="" />
        <p className="text-[#616161] font-medium text-sm leading-[18px] font-sans">Share</p>
@@ -116,14 +83,11 @@ import ItemTopic from "./ItemTopic";
      </div>
    </div>
   </div>
-        
-         )}
+ )}
         </div>
 
         )}
        </div>
-
-
-
   )
+  
 }
---
+++ src/components/UserChat.jsx
@@ -0,0 +1,53 @@
+import PropTypes from "prop-types";
+import Profile from "../assets/Profile.svg";
+import copy from "../assets/copy.svg";
+import edit from "../assets/edit.svg"
+import { useState } from "react";
+
+export default function UserChat({msg}){
+    const[editText, setEditText] = useState(msg);
+    const[isEdit, setIsEdit] = useState(false)
+    const handleEdit = () => {
+        setIsEdit(true)
+        }
+     const handleBlur = (e) => {
+        setEditText(e.target.innerText); 
+        setIsEdit(false);
+        };
+    return (
+        <div>
+                   <div className="flex gap-3 mt-6 mb-6">
+                    <img src={Profile} alt="profile" className="w-6 h-6 ml-6 " />
+                    <p className="">you</p>
+                   </div>
+                   {/* <p className="mr-6 ml-6 mt-4 text-[#01CD98] font-sans font-normal text-base leading-6">{msg}</p> */}
+                   <p
+                className={`mr-6 ml-6 mt-4 text-[#01CD98] font-sans font-normal text-base leading-6 ${
+                    isEdit ? "rounded border-1 before:content-['⭐'] text-[#000000]" : ""
+                }`}
+                contentEditable={isEdit}
+                onBlur={handleBlur} 
+            >
+                {editText}
+            </p>
+                  
+                  <div className="flex gap-6 flex-row ml-6 mt-4">
+                    <button className="flex gap-2" onClick={handleEdit}>
+                    <img className="w-4 h-4" src={edit} alt="edit" />
+                    <p className="text-[#616161] font-medium text-sm leading-[18px] font-sans"
+                     
+                    >Edit</p>
+                  </button>
+                  <div className="flex gap-2">
+                    <img className="w-4 h-4" src={copy} alt="copy" />
+                    <p className="text-[#616161] font-medium text-sm leading-[18px] font-sans">Copy</p>
+                  </div>
+                </div>
+        </div>
+    )
+}
+
+
+UserChat.propTypes ={
+    msg:PropTypes.array,
+}
